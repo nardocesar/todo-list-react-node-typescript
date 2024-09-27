01bf59fc42c343fca6ea729589a2ca84f6e7633b
Carlos Nardo <c.cesar.nardo@gmail.com>
2024-09-27 08:47:51 -0300

    chore: change port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import taskRoutes from './routes/taskRoutes';
+import express from "express";
+import taskRoutes from "./routes/taskRoutes";
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/tasks', taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
