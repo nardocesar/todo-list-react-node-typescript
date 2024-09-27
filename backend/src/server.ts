f2a571604d5a14b663c56897614a1fca8e3c51a9
Carlos Nardo <c.cesar.nardo@gmail.com>
2024-09-27 09:02:38 -0300

    chore: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import taskRoutes from "./routes/taskRoutes";
+import cors from "cors";
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5001;
