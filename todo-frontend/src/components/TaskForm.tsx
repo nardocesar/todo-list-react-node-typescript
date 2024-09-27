f36d8dd5e4858ba3358d9a56ea560646ee7010b5
Carlos Nardo <c.cesar.nardo@gmail.com>
2024-09-27 12:04:51 -0300

    chore: done
    (cherry picked from commit 17d505d87f4fb7e53ada3302f990369ed0da967f)

--- todo-frontend/src/components/TaskForm.tsx
+++ todo-frontend/src/components/TaskForm.tsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
-import { addTask } from '../services/taskService';
+import React, { useState } from "react";
+import { addTask } from "../services/taskService";
 
 const TaskForm: React.FC<{ onTaskAdded: () => void }> = ({ onTaskAdded }) => {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     await addTask(title);
-    setTitle('');
+    setTitle("");
     onTaskAdded();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="flex gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="New Task"
         required
+        className="border rounded-lg border-gray-300 py-1 px-2 text-sm"
       />
-      <button type="submit">Add Task</button>
+      <button
+        className="text-sm bg-blue-500 p-1 rounded-md text-white font-bold transition-all hover:bg-blue-600"
+        type="submit"
+      >
+        Add Task
+      </button>
     </form>
   );
 };
